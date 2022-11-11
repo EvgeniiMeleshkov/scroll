56438a581f18cf12d19b63f6de3c584035c2655f
Evgenii <emeleshkov@mail.ru>
2022-11-11 03:17:16 -0300

    almost done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
     return (
         <div className="App">
-            <header className="App-header">
+
                 <div className="container">
                     <header className="header">header</header>
 
@@ -39,7 +39,7 @@ function App() {
                                         <div className={'imgSection'}>
 
                                             <img
-                                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToeYXAu48Su42FeUe7sMTM77sms28MzbUuGg&usqp=CAU"/>
+                                                src="https://www.shutterstock.com/image-photo/cute-autumn-hygge-home-decor-600w-1812210559.jpg"/>
 
                                         </div>
                                     </div>
@@ -47,7 +47,7 @@ function App() {
                                         <div className={'imgSection'}>
 
                                             <img
-                                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtC7GF3PrlQ070iZ2wf9-bjDbhR_JpXGOMerBbs7-xDrQm7Z_gm_wVzID3uw94qebnIsA&usqp=CAU"/>
+                                                src="https://www.shutterstock.com/image-photo/home-comfort-living-room-sofa-600w-1224606409.jpg"/>
 
                                         </div>
                                     </div>
@@ -55,7 +55,7 @@ function App() {
                                         <div className={'imgSection'}>
 
                                             <img
-                                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUjTZvZG2sYFUjGFYfCquZRdt6KKdpaPnihpMs-LhH0vZCoKXh8P8dNNlhbpCgFj-Biro&usqp=CAU"/>
+                                                src="https://www.shutterstock.com/image-photo/herbal-therapy-traditional-medicine-homeopathy-600w-1924836548.jpg"/>
 
                                         </div>
                                     </div>
@@ -64,36 +64,10 @@ function App() {
                         </div>
                     </ScrollSync>
 
-
-                    {/*<ScrollSync>*/}
-                    {/*    <div style={{display: 'flex', position: 'relative', height: "100vh"}}>*/}
-                    {/*        <ScrollSyncPane>*/}
-                    {/*            <div style={{overflow: 'auto'}}>*/}
-                    {/*                <section style={{height: 1000}}>*/}
-
-                    {/*                        <div className={'main'} style={{background: 'green'}}>main</div>*/}
-                    {/*                        <div className={'main'} style={{background: 'yellow'}}>main</div>*/}
-                    {/*                        <div className={'main'} style={{background: 'red'}}>main</div>*/}
-
-                    {/*                </section>*/}
-                    {/*            </div>*/}
-                    {/*        </ScrollSyncPane>*/}
-
-                    {/*        <ScrollSyncPane>*/}
-                    {/*            <div style={{overflow: 'auto'}}>*/}
-                    {/*                <section style={{height: '100vh'}}>*/}
-
-
-                    {/*                </section>*/}
-
-                    {/*            </div>*/}
-                    {/*        </ScrollSyncPane>*/}
-                    {/*    </div>*/}
-                    {/*</ScrollSync>*/}
                     <footer className="footer">footer</footer>
                 </div>
 
-            </header>
+
         </div>
     )
 }
